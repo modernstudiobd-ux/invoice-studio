bc4535259710424ce74e9025113fd713ea0f4814
modernstudiobd-ux <modernstudiobd@gmail.com>
2026-09-15 11:59:22 +0600

    Commit & Push

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // InvoGen - Invoice Generator — Service Worker
 // Bump this version string whenever index.html (or any cached asset) changes,
 // so returning users automatically pick up the new version.
-const VERSION = "v3.17.1";
+const VERSION = "v3.18.0";
 const SHELL_CACHE = `invoice-studio-shell-${VERSION}`;
 const RUNTIME_CACHE = `invoice-studio-runtime-${VERSION}`;
 
@@ -39,6 +39,7 @@ const SHELL_ASSETS = [
   "./fonts/currency-latinext.woff2",
   "./fonts/currency-thai.woff2",
   "./fonts/currency-bengali.woff2",
+  "./icons/icon.svg",
   "./icons/icon-72.png",
   "./icons/icon-96.png",
   "./icons/icon-128.png",
@@ -47,7 +48,11 @@ const SHELL_ASSETS = [
   "./icons/icon-192.png",
   "./icons/icon-384.png",
   "./icons/icon-512.png",
-  "./icons/icon-maskable-512.png"
+  "./icons/icon-maskable-512.png",
+  "./icons/favicon-16.png",
+  "./icons/favicon-32.png",
+  "./icons/apple-touch-icon.png",
+  "./favicon.ico"
 ];
 
 self.addEventListener("install", (event) => {
